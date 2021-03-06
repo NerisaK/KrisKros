9d21af89033fccf9e8a11a7e4467cae99cd7a4ad
NerisaK <nerisakalosin@seznam.cz>
2021-03-06 14:58:27 +0100

    drag & drop added

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-const newLettersButton = document.querySelector("#newLetters");
 const boardBody = document.querySelector("#boardBody");
+const playersLetters = document.querySelectorAll(".letter");
+const newLettersButton = document.querySelector("#newLetters");
+const letterSpans = document.querySelectorAll(".lettersRow span");
+
+function generateBoard() {
+    for (let i = 1; i < 16; i++) {
+        const newRow = document.createElement("div");
+        newRow.className += "boardRow";
+        for (let i = 1; i < 16; i++) {
+            const newSquare = document.createElement("span");
+            newSquare.className += "square";
+            newRow.append(newSquare);
+        }
+        boardBody.append(newRow);
+        
+    }
+}
+
+generateBoard();
+
+const allSquares = document.querySelectorAll(".square");
 
 function randomLetter() {
     const alphabet = "aeiouyrtzplkjhgfdsxcvbnměščřžýáíéůú";
@@ -13,20 +33,29 @@ function giveLetters () {
     }    
 }
 
-function generateBoard() {
-    for (let i = 1; i < 16; i++) {
-        const newRow = document.createElement("div");
-        newRow.className += "boardRow";
-        for (let i = 1; i < 16; i++) {
-            const newSquare = document.createElement("span");
-            newSquare.className += "square";
-            newRow.append(newSquare);
-        }
-        boardBody.append(newRow);
-        giveLetters();
-    }
-}
-
-document.body.onload = generateBoard;
+giveLetters();
 
 newLettersButton.addEventListener("click", giveLetters);
+
+playersLetters.forEach(letter => {
+    letter.addEventListener("dragstart", () => {
+        letter.classList.add("dragging");      
+    })
+
+    letter.addEventListener("dragend", () => {
+        letter.classList.remove("dragging");
+    })
+})
+
+function dropElement(nodeList) {
+    nodeList.forEach(node => {
+        node.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            const letter = document.querySelector(".dragging");
+            node.appendChild(letter);
+        })
+    })
+}
+
+dropElement(allSquares);
+dropElement(letterSpans);
